55f9f8ab1bba8df7c042d20ad11dd2abb4002b31
sharutyunyan7777@gmail.com <96731429+seryozhaharutyunyan@users.noreply.github.com>
2023-03-01 18:10:41 +0300

    1.0

--- routes/routes.php
+++ routes/routes.php
@@ -5,12 +5,15 @@
 
 $this->router->add('login', "POST", '/login', 'AuthController:login');
 $this->router->add('registration', "POST", '/reg', 'AuthController:registration');
-$this->router->add('logout', 'get', '/logout', 'AuthController:logout');
+$this->router->add('logout', 'get', '/logout', 'AuthController:logout', 'auth');
 
+/**
+ * list routes orders
+ */
 $this->router->add('showOrders', 'get', '/orders', 'OrderController:index', 'auth');
-$this->router->add('addOrders', 'post', '/orders/add', 'OrderController:store', 'auth');
+$this->router->add('addOrder', 'post', '/orders/add', 'OrderController:store', 'auth');
 $this->router->add('updateOrders', 'patch', '/orders/{id:int}', 'OrderController:update', 'auth');
 $this->router->add('deleteOrders', 'delete', '/orders/{id:int}', 'OrderController:delete', 'auth');
-$this->router->add('setAllOrders', 'post', '/orders/set', 'OrderController:set', 'auth');
+$this->router->add('setAllOrders', 'get', '/orders/set', 'OrderController:set', 'auth');
 $this->router->add('setOneOrder', 'get', '/orders/setOne/{id:int}', 'OrderController:setOne', 'auth');
 
